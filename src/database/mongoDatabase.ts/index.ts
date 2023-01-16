d2aa52f4cfe15dd250aa905db2368f781bd4091b
vinicius silva <vinicius_dev@hotmail.com>
2023-01-16 04:17:39 -0300

    feat: Implementa o metodo `create` a classe MongoDatabase

--- src/database/mongoDatabase.ts/index.ts
+++ src/database/mongoDatabase.ts/index.ts
@@ -6,10 +6,16 @@ dotev.config()
 const uri = `mongodb+srv://vinicius:${process.env.PASS}@cluster0.2vla7ld.mongodb.net/?retryWrites=true&w=majority`
 const database = new MongoClient(uri).db('portifolio')
 
-export default class MongoDatabase {
+export default class MongoDatabase<T> implements CollectionMethods<T> {
   private _collection
 
   constructor(private _collectionName: string, private _database = database) {
     this._collection = this._database.collection(this._collectionName)
+  }
+
+  async create(entite: T): Promise<boolean> {
+    const data = await this._collection.insertOne(entite as unknown as OptionalId<Document>)
+    if(data.acknowledged === true) { return true }
+    return false
   }
 }
