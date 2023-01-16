718e08a9320751ae1599b089cc12b461770ec23c
vinicius silva <vinicius_dev@hotmail.com>
2023-01-16 04:15:33 -0300

    feat: Cria classe MongoDatabase

--- src/database/mongoDatabase.ts/index.ts
+++ src/database/mongoDatabase.ts/index.ts
@@ -1,7 +1,15 @@
-import { MongoClient, ObjectId, OptionalId } from 'mongodb'
+import { MongoClient } from 'mongodb'
 import dotev from 'dotenv'
 
 dotev.config()
 
 const uri = `mongodb+srv://vinicius:${process.env.PASS}@cluster0.2vla7ld.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(uri)
+const database = new MongoClient(uri).db('portifolio')
+
+export default class MongoDatabase {
+  private _collection
+
+  constructor(private _collectionName: string, private _database = database) {
+    this._collection = this._database.collection(this._collectionName)
+  }
+}
