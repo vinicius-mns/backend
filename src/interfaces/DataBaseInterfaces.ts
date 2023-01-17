e505908fdce87275b6493b613f1e046c64ac6b67
vinicius silva <vinicius_dev@hotmail.com>
2023-01-17 20:06:27 -0300

    refactor: Renomeia interfaces

--- src/interfaces/DataBaseInterfaces.ts
+++ src/interfaces/DataBaseInterfaces.ts
@@ -1,14 +1,19 @@
-export interface DB<T> {
+export interface IDataBase<T> {
   collection(name: string): {
+
     insertOne(entite: T): Promise<{acknowledged: boolean}>
+
     find(obj: unknown): {toArray(): Promise<T[]>}
+
     findOne(obj: {id: string}): Promise<T | null>
+
     updateOne(obj: {id: string},  set: {$set: T}): Promise<{acknowledged: boolean}>
+    
     deleteOne(obj: {id: string}): Promise<{acknowledged: boolean}>
   }
 }
 
-export interface CollectionMethods<T> {
+export interface ICollectionMetods<T> {
   create(entite: T): Promise<boolean>
 
   readAll(): Promise<T[]>
