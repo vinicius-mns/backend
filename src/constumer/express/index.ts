0fa8184d65ff35a18d853c9309a3a4124eb1c534
vinicius silva <vinicius_dev@hotmail.com>
2023-01-19 00:00:12 -0300

    feat: Cria classe Express

---
+++ src/constumer/express/index.ts
@@ -0,0 +1,26 @@
+import express, { Router } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env || 3002
+
+export class MyExpress {
+
+  constructor(private _express = express()){
+    this._init()
+  }
+
+  private _init() {
+    this._express.use(express.json())
+    this._express.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}/`)
+    })
+  }
+
+  addRoute(r: Router[]) {
+    r.map(router => {
+      this._express.use(router)
+    })
+  }
+}
