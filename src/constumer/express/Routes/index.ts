eae9745b7525b113a9c5684c1cde81420772e8db
vinicius silva <vinicius_dev@hotmail.com>
2023-01-19 18:30:10 -0300

    feat: Cria `readOne` em Rotas do express

--- src/constumer/express/Routes/index.ts
+++ src/constumer/express/Routes/index.ts
@@ -4,8 +4,8 @@ import { IUseCases } from '../../../interfaces/UseCasesInterface'
 export class MyRouters<T> {
   constructor(private _path: string, private _useCase: IUseCases<T>, private _router = Router()){}
   
-  create() {
-    return this._router.get(`/${this._path}/create`, async(req, res) => {
+  private _create() {
+    return this._router.post(`/${this._path}/create`, async(req, res) => {
       const body = req.body
       const data = await this._useCase.create(body)
       return res.status(data.statusCode).json(data.content)
@@ -15,6 +15,14 @@ export class MyRouters<T> {
   private _readAll() {
     return this._router.get(`/${this._path}/all`, async(_req, res) => {
       const data = await this._useCase.readAll()
+      return res.status(data.statusCode).json(data.content)
+    })
+  }
+
+  private _readOne() {
+    return this._router.get(`/${this._path}/find/:id`, async(req, res) => {
+      const { id } = req.params
+      const data = await this._useCase.readOne(id)
       return res.status(data.statusCode).json(data.content)
     })
   }
