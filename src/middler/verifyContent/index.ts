0a64a614f957897a87dfe8df49c86d64a164b2b3
vinicius silva <vinicius_dev@hotmail.com>
2023-01-25 22:04:43 -0300

    feat: Cria metodo para validar o `content` do metodo `object` da classe `Primitive`

--- src/middler/verifyContent/index.ts
+++ src/middler/verifyContent/index.ts
@@ -9,6 +9,7 @@ interface ObjectLinter {
 interface SchemaOBJ {
   [index: string ]: SafeParse
 }
+
 export class Primitive {
 
   private static valitadeWithMessage(typeName: string, key: string, type: unknown) {
@@ -17,7 +18,34 @@ export class Primitive {
     }
   }
 
-  static string() {
+  private static _verifyAllKeys(schemaObj: SchemaOBJ, contentObj: ObjectLinter) {
+    const ArrSchema = Object.keys(schemaObj)
+    const ArrContent = Object.keys(contentObj)
+    
+    const err: { message: string | undefined, error: boolean } = { message: undefined, error: false }
+
+    if( ArrSchema.length > ArrContent.length ){
+      for ( const key of ArrSchema ) {
+        if(!contentObj[key]) {
+          err.message = `Está faltando a chave "${key}"`
+          err.error = true
+        }
+      }
+    }
+
+    if( ArrContent.length > ArrSchema.length ) {
+      for ( const key of ArrContent ) {
+        if(!schemaObj[key]) {
+          err.message = `A chave: "${key}" nao existe`
+          err.error = true
+        }
+      }
+    }
+    
+    return err
+  }
+
+  static object(obj: SchemaOBJ) {
     return {
       validate: (key:string, type: string, typeName='string') => this.valitadeWithMessage(typeName, key, type)
     }
