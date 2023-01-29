bd85cdaa439ed640d830004e92cdae3be995d34f
vinicius silva <vinicius_dev@hotmail.com>
2023-01-29 19:52:50 -0300

    feat: Adiciona mockdbFailued para test do `useCasesCrude`

--- src/useCases/CRUDE/crud.test.ts
+++ src/useCases/CRUDE/crud.test.ts
@@ -21,3 +21,23 @@ class mockSuccessDb<T> implements ICollectionMethods<T> {
     throw new Error('Method not implemented.')
   }
 }
+
+class mockFailureDb<T> implements ICollectionMethods<T> {
+
+  create(entite: T): Promise<boolean> {
+    return new Promise((resolve,) => resolve(false))
+  }
+
+  readAll(): Promise<T[]> {
+    throw new Error('Method not implemented.')
+  }
+  readOne(id: string | number): Promise<T | null> {
+    throw new Error('Method not implemented.')
+  }
+  updateOne(id: string | number, entite: T): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+  delete(id: string | number): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
