3b6a63b8778edb37b704196647ec84c4cfb2b0bf
vinicius silva <vinicius_dev@hotmail.com>
2023-01-29 19:50:11 -0300

    feat: Inicia test para `useCasesCrude`

---
+++ src/useCases/CRUDE/crud.test.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { describe, expect, it } from 'vitest'
+import { UseCasesCRUDE } from '.'
+import { ICollectionMethods } from '../../interfaces/DataBaseInterfaces'
