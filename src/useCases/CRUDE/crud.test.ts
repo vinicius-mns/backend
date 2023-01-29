5f1ccd01b8e56a8394588d4f07c5786e70b1e059
vinicius silva <vinicius_dev@hotmail.com>
2023-01-29 19:52:13 -0300

    feat: Adiciona mockdbSuccess para test do `useCasesCrude`

--- src/useCases/CRUDE/crud.test.ts
+++ src/useCases/CRUDE/crud.test.ts
@@ -2,3 +2,22 @@
 import { describe, expect, it } from 'vitest'
 import { UseCasesCRUDE } from '.'
 import { ICollectionMethods } from '../../interfaces/DataBaseInterfaces'
+class mockSuccessDb<T> implements ICollectionMethods<T> {
+
+  create(entite: T): Promise<boolean> {
+    return new Promise((resolve,) => resolve(true))
+  }
+
+  readAll(): Promise<T[]> {
+    throw new Error('Method not implemented.')
+  }
+  readOne(id: string | number): Promise<T | null> {
+    throw new Error('Method not implemented.')
+  }
+  updateOne(id: string | number, entite: T): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+  delete(id: string | number): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
