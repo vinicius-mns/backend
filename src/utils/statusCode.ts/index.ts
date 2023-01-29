bc3a0e7a1d4bc24ba1472cac69b0ba86fb0bd530
vinicius silva <vinicius_dev@hotmail.com>
2023-01-29 19:10:37 -0300

    feat: `create` utils  `StatusCode`

--- src/utils/statusCode.ts/index.ts
+++ src/utils/statusCode.ts/index.ts
@@ -2,6 +2,10 @@ import { IhttpResponse } from '../../interfaces/UseCasesInterface'
 
 export class StatusCode {
 
+  static created<T>( content:T ) {
+    return { statusCode: 201, content }
+  }
+
   static create<T>(statusCode: number, content:T): IhttpResponse<T> {
     return { statusCode, content }
   }
